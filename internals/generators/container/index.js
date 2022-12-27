6d99e865430b884333c867ea182541783ee6b8d5
Sourav <sourav.singh@emoha.com>
2022-12-27 21:37:06 +0530

    fix:plop fix

--- internals/generators/container/index.js
+++ internals/generators/container/index.js
@@ -2,77 +2,77 @@
  * Container Generator
  */
 
-const componentExists = require('../utils/componentExists');
+const componentExists = require("../utils/componentExists");
 
 module.exports = {
-  description: 'Add a container component',
+  description: "Add a container component",
   prompts: [
     {
-      type: 'input',
-      name: 'name',
-      message: 'What should it be called?',
-      default: 'Form',
-      validate: value => {
+      type: "input",
+      name: "name",
+      message: "What should it be called?",
+      default: "Form",
+      validate: (value) => {
         if (/.+/.test(value)) {
           return componentExists(value)
-            ? 'A component or container with this name already exists'
+            ? "A component or container with this name already exists"
             : true;
         }
 
-        return 'The name is required';
+        return "The name is required";
       },
     },
     {
-      type: 'confirm',
-      name: 'memo',
+      type: "confirm",
+      name: "memo",
       default: false,
-      message: 'Do you want to wrap your component in React.memo?',
+      message: "Do you want to wrap your component in React.memo?",
     },
     {
-      type: 'confirm',
-      name: 'wantHeaders',
+      type: "confirm",
+      name: "wantHeaders",
       default: false,
-      message: 'Do you want headers?',
+      message: "Do you want headers?",
     },
     {
-      type: 'confirm',
-      name: 'wantActionsAndReducer',
+      type: "confirm",
+      name: "wantActionsAndReducer",
       default: true,
       message:
-        'Do you want an actions/constants/selectors/reducer tuple for this container?',
+        "Do you want an actions/constants/selectors/reducer tuple for this container?",
     },
     {
-      type: 'confirm',
-      name: 'wantSaga',
+      type: "confirm",
+      name: "wantSaga",
       default: true,
-      message: 'Do you want sagas for asynchronous flows? (e.g. fetching data)',
+      message: "Do you want sagas for asynchronous flows? (e.g. fetching data)",
     },
     {
-      type: 'confirm',
-      name: 'wantMessages',
+      type: "confirm",
+      name: "wantMessages",
       default: true,
-      message: 'Do you want i18n messages (i.e. will this component use text)?',
+      message: "Do you want i18n messages (i.e. will this component use text)?",
     },
     {
-      type: 'confirm',
-      name: 'wantLoadable',
+      type: "confirm",
+      name: "wantLoadable",
       default: true,
-      message: 'Do you want to load resources asynchronously?',
+      message: "Do you want to load resources asynchronously?",
     },
   ],
-  actions: data => {
+  actions: (data) => {
     // Generate index.js and index.test.js
     const actions = [
       {
-        type: 'add',
-        path: '../../app/containers/{{properCase name}}/index.js',
-        templateFile: './container/index.js.hbs',
+        type: "add",
+        path: "../../src/pages/{{properCase name}}/index.js",
+        templateFile: "./container/index.js.hbs",
         abortOnFail: true,
       },
       {
-        type: 'add',
-        path: '../../app/containers/{{properCase name}}/tests/index.test.js',
-        templateFile: './container/test.js.hbs',
+        type: "add",
+        path: "../../src/pages/{{properCase name}}/tests/index.test.js",
+        templateFile: "./container/test.js.hbs",
         abortOnFail: true,
       },
     ];
@@ -80,9 +80,9 @@ module.exports = {
     // If component wants messages
     if (data.wantMessages) {
       actions.push({
-        type: 'add',
-        path: '../../app/containers/{{properCase name}}/messages.js',
-        templateFile: './container/messages.js.hbs',
+        type: "add",
+        path: "../../src/pages/{{properCase name}}/messages.js",
+        templateFile: "./container/messages.js.hbs",
         abortOnFail: true,
       });
     }
@@ -92,52 +92,51 @@ module.exports = {
     if (data.wantActionsAndReducer) {
       // Actions
       actions.push({
-        type: 'add',
-        path: '../../app/containers/{{properCase name}}/actions.js',
-        templateFile: './container/actions.js.hbs',
+        type: "add",
+        path: "../../src/pages/{{properCase name}}/actions.js",
+        templateFile: "./container/actions.js.hbs",
         abortOnFail: true,
       });
       actions.push({
-        type: 'add',
-        path: '../../app/containers/{{properCase name}}/tests/actions.test.js',
-        templateFile: './container/actions.test.js.hbs',
+        type: "add",
+        path: "../../src/pages/{{properCase name}}/tests/actions.test.js",
+        templateFile: "./container/actions.test.js.hbs",
         abortOnFail: true,
       });
 
       // Constants
       actions.push({
-        type: 'add',
-        path: '../../app/containers/{{properCase name}}/constants.js',
-        templateFile: './container/constants.js.hbs',
+        type: "add",
+        path: "../../src/pages/{{properCase name}}/constants.js",
+        templateFile: "./container/constants.js.hbs",
         abortOnFail: true,
       });
 
       // Selectors
       actions.push({
-        type: 'add',
-        path: '../../app/containers/{{properCase name}}/selectors.js',
-        templateFile: './container/selectors.js.hbs',
+        type: "add",
+        path: "../../src/pages/{{properCase name}}/selectors.js",
+        templateFile: "./container/selectors.js.hbs",
         abortOnFail: true,
       });
       actions.push({
-        type: 'add',
-        path:
-          '../../app/containers/{{properCase name}}/tests/selectors.test.js',
-        templateFile: './container/selectors.test.js.hbs',
+        type: "add",
+        path: "../../src/pages/{{properCase name}}/tests/selectors.test.js",
+        templateFile: "./container/selectors.test.js.hbs",
         abortOnFail: true,
       });
 
       // Reducer
       actions.push({
-        type: 'add',
-        path: '../../app/containers/{{properCase name}}/reducer.js',
-        templateFile: './container/reducer.js.hbs',
+        type: "add",
+        path: "../../src/pages/{{properCase name}}/reducer.js",
+        templateFile: "./container/reducer.js.hbs",
         abortOnFail: true,
       });
       actions.push({
-        type: 'add',
-        path: '../../app/containers/{{properCase name}}/tests/reducer.test.js',
-        templateFile: './container/reducer.test.js.hbs',
+        type: "add",
+        path: "../../src/pages/{{properCase name}}/tests/reducer.test.js",
+        templateFile: "./container/reducer.test.js.hbs",
         abortOnFail: true,
       });
     }
@@ -145,31 +144,31 @@ module.exports = {
     // Sagas
     if (data.wantSaga) {
       actions.push({
-        type: 'add',
-        path: '../../app/containers/{{properCase name}}/saga.js',
-        templateFile: './container/saga.js.hbs',
+        type: "add",
+        path: "../../src/pages/{{properCase name}}/saga.js",
+        templateFile: "./container/saga.js.hbs",
         abortOnFail: true,
       });
       actions.push({
-        type: 'add',
-        path: '../../app/containers/{{properCase name}}/tests/saga.test.js',
-        templateFile: './container/saga.test.js.hbs',
+        type: "add",
+        path: "../../src/pages/{{properCase name}}/tests/saga.test.js",
+        templateFile: "./container/saga.test.js.hbs",
         abortOnFail: true,
       });
     }
 
     if (data.wantLoadable) {
       actions.push({
-        type: 'add',
-        path: '../../app/containers/{{properCase name}}/Loadable.js',
-        templateFile: './component/loadable.js.hbs',
+        type: "add",
+        path: "../../src/pages/{{properCase name}}/Loadable.js",
+        templateFile: "./component/loadable.js.hbs",
         abortOnFail: true,
       });
     }
 
     actions.push({
-      type: 'prettify',
-      path: '/containers/',
+      type: "prettify",
+      path: "/pages/",
     });
 
     return actions;
--- internals/generators/utils/componentExists.js
+++ internals/generators/utils/componentExists.js
@@ -4,13 +4,13 @@
  * Check whether the given component exist in either the components or containers directory
  */
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const pageComponents = fs.readdirSync(
-  path.join(__dirname, '../../../app/components'),
+  path.join(__dirname, "../../../src/components")
 );
 const pageContainers = fs.readdirSync(
-  path.join(__dirname, '../../../app/containers'),
+  path.join(__dirname, "../../../src/pages")
 );
 const components = pageComponents.concat(pageContainers);
 
